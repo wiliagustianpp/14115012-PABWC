2917330435031c452318a32cfafbc11f67bc94ca
wiliagustianpp <35952693+wiliagustianpp@users.noreply.github.com>
2019-10-24 10:24:19 +0700

    minggu-6

---
+++ Pertemuan-6/Latihan1/OperatorPenugasan.php
@@ -0,0 +1,10 @@
+<?php 
+$a = 3;
+$b = 7;
+$a += 5;
+$b = ($c = 11) + 3;
+
+echo "Nilai variable a adalah = $a<br/>";
+echo "Nilai variable b adalah = $b<br/>";
+echo "Nilai variable c adalah = $c<br/>";
+ ?>
---
+++ Pertemuan-6/Latihan1/PemakaianVariable.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contoh Script PHP</title>
+</head>
+<body>
+    <?php 
+    $nama = "Kartosuro";
+    echo "$nama adalah \"Driver Go Mart\"";
+    ?>
+</body>
+</html>
