a14fc6a012e9bcd8e082a1e2831c1e2e736afb5d
wiliagustianpp <35952693+wiliagustianpp@users.noreply.github.com>
2019-11-14 10:32:26 +0700

    minggu-10

---
+++ minggu-10/cookies/hapus_cookies.php
@@ -0,0 +1,10 @@
+<?php
+// set masa berlakunya menjadi sejam yang lalu (sudah lewat) 
+setcookie("user", "", time() - 3600);
+?>
+<html>
+
+<body> <?php echo "Cookie 'user' terhapus.";
+        10. ?> </body>
+
+</html>
